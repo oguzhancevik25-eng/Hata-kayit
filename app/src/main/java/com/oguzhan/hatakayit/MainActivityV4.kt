2591d939de3ea5041153ed5a30d31e8e2d6493ab
oguzhancevik25-eng <oguzhancevik25@gmail.com>
2026-09-17 16:15:59 +0300

    Fix v1.3 Compose state list import

--- app/src/main/java/com/oguzhan/hatakayit/MainActivityV4.kt
+++ app/src/main/java/com/oguzhan/hatakayit/MainActivityV4.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
