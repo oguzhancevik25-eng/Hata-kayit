2758428cee87e91da3b90e87002eb5ecc6da995d
oguzhancevik25-eng <oguzhancevik25@gmail.com>
2026-09-17 16:31:25 +0300

    Windows PC sürümü
    Operatör Takip Windows portable EXE sürümünü ekler.

---
+++ desktop/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1500,
+    height: 920,
+    minWidth: 1100,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    backgroundColor: '#f5f7fa',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
