964fbf017413c6b70564655b342c5ef3865a6076
oguzhancevik25-eng <oguzhancevik25@gmail.com>
2026-09-18 06:29:38 +0300

    Add desktop JavaScript syntax check

---
+++ desktop/check-syntax.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const vm = require('vm');
+
+for (const file of ['v15-extra.js']) {
+  const source = fs.readFileSync(file, 'utf8');
+  new vm.Script(source, { filename: file });
+}
+
+const html = fs.readFileSync('index.html', 'utf8');
+const scripts = [...html.matchAll(/<script(?![^>]*\bsrc=)[^>]*>([\s\S]*?)<\/script>/gi)];
+if (!scripts.length) throw new Error('index.html inline script bulunamadı');
+scripts.forEach((m, i) => new vm.Script(m[1], { filename: 'index-inline-' + (i + 1) + '.js' }));
+console.log('JavaScript syntax OK');
