25be92d34fe064849b4b1a25ff5f5198975469f9
oguzhancevik25-eng <oguzhancevik25@gmail.com>
2026-09-18 09:30:28 +0300

    Bump Android version to 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.oguzhan.hatakayit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
     }
 
     buildFeatures {
